078f09177e3346d978fdcee600c0eaf1814ebd18
drummyfish <tastyfish@seznam.cz>
2017-05-07 14:40:08 +0200

    more stuff

--- upscale.py
+++ upscale.py
@@ -1,5 +1,16 @@
 from PIL import Image
 
+# implementation of many filters can be found at:
+#
+# https://code.google.com/archive/p/2dimagefilter/downloads?page=1
+#
+# a good algorithm:
+#
+# paper: http://johanneskopf.de/publications/pixelart/paper/pixel.pdf
+# demo and resources: http://johanneskopf.de/publications/pixelart/supplementary/index.html
+#
+#=======================================================================
+
 # helper functions:
 
 # https://en.wikipedia.org/wiki/YUV
@@ -10,6 +21,11 @@ def rgb_to_yuv(pixel_rgb):
 
 def compare_pixels_exact(pixel1, pixel2):
   return pixel1[0] == pixel2[0] and pixel1[1] == pixel2[1] and pixel1[2] == pixel2[2]
+
+def compare_pixels_yuv(pixel1, pixel2, thresh_y = 0.2, thresh_u = 0.5, thresh_v = 0.5):
+  pixel1_yuv = rgb_to_yuv(pixel1)
+  pixel2_yuv = rgb_to_yuv(pixel2)
+  return 1 if abs(pixel1_yuv[0] - pixel2_yuv[0]) < thresh_y and abs(pixel1_yuv[1] - pixel2_yuv[1]) < thresh_u and abs(pixel1_yuv[2] - pixel2_yuv[2]) < thresh_v else 0
 
 def add_pixels(pixel_list):
   result = [0,0,0]
@@ -101,7 +117,7 @@ def lines_2x(image):
   return upscale_n_times(image,2,func,1)
 
 # https://github.com/amadvance/scale2x/blob/master/scale2x.c
-# https://en.wikipedia.org/wiki/Pixel_art_scaling_algorithms#EPX.2FScale2.C3.97.2FAdvMAME2.C3.97
+# https://en.wikipedia.org/wiki/Pixel_art_scaling_algorithms#EPX.2FScale2.d.97.2FAdvMAME2.d.97
 
 def scale_2x(image):
   def func(pixels, coords):
@@ -125,7 +141,7 @@ def scale_2x(image):
   return upscale_n_times(image,2,func,1)
 
 # https://github.com/amadvance/scale2x/blob/master/scale3x.c
-# https://en.wikipedia.org/wiki/Pixel_art_scaling_algorithms#EPX.2FScale2.C3.97.2FAdvMAME2.C3.97
+# https://en.wikipedia.org/wiki/Pixel_art_scaling_algorithms#EPX.2FScale2.d.97.2FAdvMAME2.d.97
 
 def scale_3x(image):
   def func(pixels, coords):
@@ -175,11 +191,47 @@ def eagle_2x(image):
 
   return upscale_n_times(image,2,func,1)
 
+# eagle x3 algorithm found at
+# https://code.google.com/archive/p/2dimagefilter/source/default/source?page=2
+
+def eagle_3x(image):
+  def func(pixels, coords):
+    a = pixels[-1][-1]
+    b = pixels[0][-1]
+    c = pixels[1][-1]
+    d = pixels[-1][0]
+    e = pixels[0][0]
+    f = pixels[1][0]
+    g = pixels[-1][1]
+    h = pixels[0][1]
+    i = pixels[1][1]
+
+    return (
+        (
+          mix_pixels([a,b,d]) if compare_pixels_exact(a,b) and compare_pixels_exact(a,d) else e,
+          mix_pixels([ mix_pixels([a,b,d]), mix_pixels([c,b,f]) ]) if compare_pixels_exact(a,b) and compare_pixels_exact(a,d) and compare_pixels_exact(c,b) and compare_pixels_exact(c,f) else e,
+          mix_pixels([c,b,f]) if compare_pixels_exact(c,b) and compare_pixels_exact(c,f) else e
+        ),
+        (
+          mix_pixels([ mix_pixels([a,b,d]), mix_pixels([g,d,h]) ]) if compare_pixels_exact(a,b) and compare_pixels_exact(a,d) and compare_pixels_exact(g,h) and compare_pixels_exact(g,d) else e,
+          e,
+          mix_pixels([ mix_pixels([c,b,f]), mix_pixels([i,f,h]) ]) if compare_pixels_exact(c,b) and compare_pixels_exact(c,f) and compare_pixels_exact(i,f) and compare_pixels_exact(i,h) else e
+        ),
+        (
+
+          mix_pixels([g,d,h]) if compare_pixels_exact(g,d) and compare_pixels_exact(g,h) else e,
+          mix_pixels([ mix_pixels([g,h,d]), mix_pixels([i,f,h]) ]) if compare_pixels_exact(g,h) and compare_pixels_exact(g,d) and compare_pixels_exact(i,f) and compare_pixels_exact(i,h) else e,
+          mix_pixels([i,f,h]) if compare_pixels_exact(i,f) and compare_pixels_exact(i,h) else e
+        )
+      )
+
+  return upscale_n_times(image,3,func,1)
+
 # https://news.ycombinator.com/item?id=7925671
 # http://pastebin.com/raw/DsNupdbc
 # https://github.com/Arcnor/hqx-java/blob/master/src/hqx/Hqx_2x.java
 
-def hq_2x(image, thresh_y = 0.5, thresh_u = 0.5, thresh_v = 0.5):
+def hq_2x(image):
   hq_table = (
       4, 4, 6,  2, 4, 4, 6,  2, 5,  3, 15, 12, 5,  3, 17, 13,
       4, 4, 6, 18, 4, 4, 6, 18, 5,  3, 12, 12, 5,  3,  1, 12,
@@ -198,11 +250,6 @@ def hq_2x(image, thresh_y = 0.5, thresh_u = 0.5, thresh_v = 0.5):
       4, 4, 6,  2, 4, 4, 6,  2, 5,  3, 16, 12, 5,  3,  1, 14,
       4, 4, 6,  2, 4, 4, 6,  2, 5,  3,  1, 12, 5,  3,  1, 14
     )
-
-  def pixels_are_far(pixel1, pixel2):
-    pixel1_yuv = rgb_to_yuv(pixel1)
-    pixel2_yuv = rgb_to_yuv(pixel2)
-    return 1 if abs(pixel1_yuv[0] - pixel2_yuv[0]) >= thresh_y or abs(pixel1_yuv[1] - pixel2_yuv[1]) >= thresh_u or abs(pixel1_yuv[2] - pixel2_yuv[2]) >= thresh_v else 0
 
   def helper_blend(rule, e, a, b, d, f, h):
     if rule == 0:
@@ -230,21 +277,21 @@ def hq_2x(image, thresh_y = 0.5, thresh_u = 0.5, thresh_v = 0.5):
     elif rule == 11:
       return mix_pixels([e,e,e,e,e,e,e,e,e,e,e,e,e,e,d,b])
     elif rule == 12:
-      return mix_pixels([e,e,d,b]) if not pixels_are_far(b,d) else e
+      return mix_pixels([e,e,d,b]) if compare_pixels_yuv(b,d) else e
     elif rule == 13:
-      return mix_pixels([e,e,d,d,d,b,b,b]) if not pixels_are_far(b,d) else e
+      return mix_pixels([e,e,d,d,d,b,b,b]) if compare_pixels_yuv(b,d) else e
     elif rule == 14:
-      return mix_pixels([e,e,e,e,e,e,e,e,e,e,e,e,e,e,d,b]) if not pixels_are_far(b,d) else e
+      return mix_pixels([e,e,e,e,e,e,e,e,e,e,e,e,e,e,d,b]) if compare_pixels_yuv(b,d) else e
     elif rule == 15:
-      return mix_pixels([e,e,d,b]) if not pixels_are_far(b,d) else mix_pixels([e,e,e,a])
+      return mix_pixels([e,e,d,b]) if compare_pixels_yuv(b,d) else mix_pixels([e,e,e,a])
     elif rule == 16:
-      return mix_pixels([e,e,e,e,e,e,d,b]) if not pixels_are_far(b,d) else mix_pixels([e,e,e,a])
+      return mix_pixels([e,e,e,e,e,e,d,b]) if compare_pixels_yuv(b,d) else mix_pixels([e,e,e,a])
     elif rule == 17:
-      return mix_pixels([e,e,d,d,d,b,b,b]) if not pixels_are_far(b,d) else mix_pixels([e,e,e,a])
+      return mix_pixels([e,e,d,d,d,b,b,b]) if compare_pixels_yuv(b,d) else mix_pixels([e,e,e,a])
     elif rule == 18:
-      return mix_pixels([e,e,e,e,e,b,b,d]) if not pixels_are_far(b,f) else mix_pixels([e,e,e,d])
+      return mix_pixels([e,e,e,e,e,b,b,d]) if compare_pixels_yuv(b,f) else mix_pixels([e,e,e,d])
     else:
-      return mix_pixels([e,e,e,e,e,d,d,b]) if not pixels_are_far(d,h) else mix_pixels([e,e,e,b])
+      return mix_pixels([e,e,e,e,e,d,d,b]) if compare_pixels_yuv(d,h) else mix_pixels([e,e,e,b])
 
   def rotate_pattern(pattern):
     return (pattern[2], pattern[4], pattern[7], pattern[1], pattern[6], pattern[0], pattern[3], pattern[5])
@@ -271,14 +318,14 @@ def hq_2x(image, thresh_y = 0.5, thresh_u = 0.5, thresh_v = 0.5):
     i = pixels[1][1]
 
     pattern = (
-      pixels_are_far(e,i),
-      pixels_are_far(e,h),
-      pixels_are_far(e,g),
-      pixels_are_far(e,f),
-      pixels_are_far(e,d),
-      pixels_are_far(e,c),
-      pixels_are_far(e,b),
-      pixels_are_far(e,a),
+      not compare_pixels_yuv(e,i),
+      not compare_pixels_yuv(e,h),
+      not compare_pixels_yuv(e,g),
+      not compare_pixels_yuv(e,f),
+      not compare_pixels_yuv(e,d),
+      not compare_pixels_yuv(e,c),
+      not compare_pixels_yuv(e,b),
+      not compare_pixels_yuv(e,a),
       )
 
     p0 = helper_blend(hq_table[pattern_to_number(pattern)], e, a, b, d, f, h)
@@ -293,11 +340,15 @@ def hq_2x(image, thresh_y = 0.5, thresh_u = 0.5, thresh_v = 0.5):
 
   return upscale_n_times(image,2,func,1)
 
+#---------------------------------------
+
 image = Image.open("test.png")
+
 nearest_neighbour_2x(image).save("nearest.png","PNG")
 linear_2x(image).save("linear.png","PNG")
 lines_2x(image).save("lines.png","PNG")
 eagle_2x(image).save("eagle.png","PNG")
+eagle_3x(image).save("eagle3.png","PNG")
 scale_2x(image).save("scale_2x.png","PNG")
 scale_3x(image).save("scale_3x.png","PNG")
 hq_2x(image).save("hq2x.png","PNG")
